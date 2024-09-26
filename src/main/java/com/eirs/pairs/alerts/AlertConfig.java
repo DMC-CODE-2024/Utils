5ac7e1a8f3e1cba244c6d8e763990d3e60a43e50
Arun Mehta <arun271333@gmail.com>
2024-09-26 21:12:09 +0530

    Common Alerts Changes

--- src/main/java/com/eirs/pairs/alerts/AlertConfig.java
+++ src/main/java/com/eirs/pairs/alerts/AlertConfig.java
@@ -1,22 +1,33 @@
 package com.eirs.pairs.alerts;
 
 import com.eirs.pairs.alerts.constants.AlertIds;
-import com.eirs.pairs.constants.UtilityType;
 import jakarta.annotation.PostConstruct;
 import lombok.Data;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @Data
-@ConfigurationProperties(prefix = "eirs.alert")
 public class AlertConfig {
 
+    @Value("${eirs.alert.url}")
     private String url;
 
     private Map<AlertIds, AlertConfigDto> alertsMapping;
+
+    @PostConstruct
+    public void init() {
+        alertsMapping = new HashMap<>();
+        alertsMapping = new HashMap<>();
+        alertsMapping.put(AlertIds.CONFIGURATION_VALUE_MISSING, new AlertConfigDto("alert2201"));
+        alertsMapping.put(AlertIds.CONFIGURATION_VALUE_WRONG, new AlertConfigDto("alert2202"));
+        alertsMapping.put(AlertIds.SMS_VALUE_MISSING, new AlertConfigDto("alert2203"));
+        alertsMapping.put(AlertIds.MODULE_EXECUTED_WITH_EXCEPTION, new AlertConfigDto("alert2204"));
+        alertsMapping.put(AlertIds.NOTIFICATION_SEND_EXCEPTION, new AlertConfigDto("alert2205"));
+        alertsMapping.put(AlertIds.DATABASE_EXCEPTION, new AlertConfigDto("alert2206"));
+        alertsMapping.put(AlertIds.DATABASE_TABLE_EXCEPTION, new AlertConfigDto("alert2206"));
+    }
 }
--- src/main/java/com/eirs/pairs/alerts/AlertConfigDto.java
+++ src/main/java/com/eirs/pairs/alerts/AlertConfigDto.java
@@ -1,9 +1,12 @@
 package com.eirs.pairs.alerts;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AlertConfigDto {
     String alertId;
-    String message;
 }
